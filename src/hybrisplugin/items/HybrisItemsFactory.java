c0fd5908cac85cc8a0afac849abf9250cf3e5a45
Paweł Łabuda <emc206@gmail.com>
2014-10-27 10:03:31 +0100

    navigating to the line of items.xml file works

--- src/hybrisplugin/items/HybrisItemsFactory.java
+++ src/hybrisplugin/items/HybrisItemsFactory.java
@@ -1,49 +1,32 @@
 package hybrisplugin.items;
 
-import com.intellij.lang.StdLanguages;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.psi.search.FilenameIndex;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.xml.XmlFile;
-import com.intellij.psi.xml.XmlTag;
-import com.intellij.ui.components.JBScrollPane;
-import com.intellij.ui.content.impl.ContentImpl;
-import hybrisplugin.items.xml.tree.XmlTree;
-import hybrisplugin.items.xml.tree.XmlTreeModel;
-
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
-
 /**
  * @author Paweł Łabuda
  */
-public class HybrisItemsFactory implements ToolWindowFactory
+public class HybrisItemsFactory
+//        implements ToolWindowFactory
 {
-   @Override
-   public void createToolWindowContent(final Project project, final ToolWindow toolWindow)
-   {
-      final XmlFile opocoreItems = (XmlFile)FilenameIndex.getFilesByName(project, "opocore-items.xml", GlobalSearchScope.allScope(project))[0].getViewProvider().getPsi(StdLanguages.XML);
-
-      XmlTreeModel xmlTreeModel = new XmlTreeModel(opocoreItems.getRootTag());
-      XmlTree xmlTree = new XmlTree(xmlTreeModel);
-
-      xmlTree.addTreeSelectionListener(new TreeSelectionListener()
-      {
-         @Override
-         public void valueChanged(TreeSelectionEvent e)
-         {
-            XmlTag currentXmlTag = (XmlTag) e.getPath().getLastPathComponent();
-
-            OpenFileDescriptor descriptor = new OpenFileDescriptor(project, opocoreItems.getVirtualFile(), currentXmlTag.getTextOffset());
-            FileEditorManager.getInstance(project).openTextEditor(descriptor, true);
-         }
-      });
-
-      toolWindow.getContentManager().addContent(new ContentImpl(new JBScrollPane(xmlTree), "Items", false));
-   }
+//   @Override
+//   public void createToolWindowContent(final Project project, final ToolWindow toolWindow)
+//   {
+//      final XmlFile opocoreItems = (XmlFile)FilenameIndex.getFilesByName(project, "opocore-items.xml", GlobalSearchScope.allScope(project))[0].getViewProvider().getPsi(StdLanguages.XML);
+//
+//      XmlTreeModel xmlTreeModel = new XmlTreeModel(opocoreItems.getRootTag());
+//      XmlTree xmlTree = new XmlTree(xmlTreeModel);
+//
+//      xmlTree.addTreeSelectionListener(new TreeSelectionListener()
+//      {
+//         @Override
+//         public void valueChanged(TreeSelectionEvent e)
+//         {
+//            XmlTag currentXmlTag = (XmlTag) e.getPath().getLastPathComponent();
+//
+//            OpenFileDescriptor descriptor = new OpenFileDescriptor(project, opocoreItems.getVirtualFile(), currentXmlTag.getTextOffset());
+//            FileEditorManager.getInstance(project).openTextEditor(descriptor, true);
+//         }
+//      });
+//
+//      toolWindow.getContentManager().addContent(new ContentImpl(new JBScrollPane(xmlTree), "Items", false));
+//   }
 
 }
